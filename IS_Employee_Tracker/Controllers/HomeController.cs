eb2e2a59b853330be485aa8590375067260be906
Tyler Curtis <tyscurtis@gmail.com>
2022-10-19 20:10:22 -0600

    Add project files.

---
+++ IS_Employee_Tracker/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using IS_Employee_Tracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IS_Employee_Tracker.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // This is our Dashboard page
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // This is where you input new records
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        // This displays a table that you can filter
+        public IActionResult Display()
+        {
+            return View();
+        }
+
+        public IActionResult Team()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ IS_Employee_Tracker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IS_Employee_Tracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
